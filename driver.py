52fa1e1b2c22668937a7b9dfe1ac9c20aaeac151
yf0361 <yf0361@lnx-wpt008-33.lunet.lboro.ac.uk>
2026-03-13 12:04:31 +0000

    set up

--- driver.py
+++ driver.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-from student1.featureA import *
-from student2.featureB import *
+from mohammed.featureA import *
+from yoyo.featureB import *
 
 print("*** Starting the driver script ***\n")
 
 # Call hello_world from student1.featureA
 hello_world()
+sub2()
 
 # Initialize the class from student2.featureB
 mc = myclass(5)
--- student1/featureA.py
+++
@@ -1,9 +0,0 @@
-"""
-Example code that contains a subroutine.
-"""
-
-def hello_world():
-    """
-    This routine says hello to the world.
-    """
-    print("Hello World!")
